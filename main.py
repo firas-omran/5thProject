1231b1ee280d2fd26d1e8aec41496254d6a62c40
firas-omran <f>
2022-05-20 14:00:26 +0300

    secoundcommit edit injection (undefined)

--- main.py
+++ main.py
@@ -295,12 +295,10 @@ def clustering(url, n_zone, max_iter):
     return zon
 
 # url = 'http://webcode.me'
-
 # url='https://lavasoft.gosearchresults.com/?q=store+sorted+dictionary+python&tt=vmn__webcompa__1_0__go__lvs__webcompa__1_0__go__ch_WCYID10270_200711__yrff__yrff&pid=5ac784309091147a162b4431'
-
 #url = 'https://eclass.hiast.edu.sy'
-# url= 'https://www.w3schools.com/'
-url = 'http://www.cad.zju.edu.cn/home/dengcai/VIPS/VIPS.html'
+url= 'https://www.w3schools.com/'
+# url = 'http://www.cad.zju.edu.cn/home/dengcai/VIPS/VIPS.html'
 # url = 'https://www1.se.cuhk.edu.hk/~textmine/code/segmentation/' 5
 # url = 'https://www.researchgate.net/publication/333576545_Alternative_Nonvisual_Web_Browsing_Techniques' #6
 # url = 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5708566/' 8 child
@@ -317,10 +315,6 @@ for i in range(0, len(zon)):
         width.append(zon[i][i]['width'])
         height.append(zon[i][i]['height'])
 
-# print(left)
-# print(top)
-# print(width)
-# print(height)
 
 driver.execute_script(open("drawborder.js").read(), left, top, width, height)
 for i in range(0, len(zon)):
